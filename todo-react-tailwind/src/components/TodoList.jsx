8834f7e999311a873f76ab428f3faa32cb6fe10c
Jupkobe <eyupglt42@gmail.com>
2023-10-31 16:23:54 +0300

    typo fix

--- todo-react-tailwind/src/components/TodoList.jsx
+++ todo-react-tailwind/src/components/TodoList.jsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from 'react';
 
 export default function TodoList({ todoList, deleteTodo, toggleTodo }) {
     const todoElems = todoList.map((todo) => {
-        return (<div key={todo.id} id={todo.id} onClick={(e) => {    
-            if (e.target.tagName !== "DIV" && e.target.tagName !== "P") return;
-            toggleTodo(todo.id);
-        }} className={todo.completed ? "todo completed" : "todo"}>
-            <p>{todo.title}</p>
-            <button onClick={(e) => {
-                deleteTodo(todo.id);
-                }} 
-                className="bg-transparent text-2xl border-0 before:content-['\2715']"></button>
-        </div>)
+        return (
+        <div 
+            key={todo.id} 
+            id={todo.id} 
+            onClick={(e) => {    
+                if (e.target.tagName !== "DIV" && e.target.tagName !== "P") return;
+                toggleTodo(todo.id);
+            }} 
+            className={todo.completed ? "todo completed" : "todo"}>
+                <p>{todo.title}</p>
+                <button
+                    onClick={() => deleteTodo(todo.id)}
+                    className="bg-transparent text-2xl border-0 before:content-['\2715']">
+                </button>
+        </div>
+        )
     });
     
 
@@ -22,15 +28,3 @@ export default function TodoList({ todoList, deleteTodo, toggleTodo }) {
         </div>
     )
 }
-
-
-
-
-
-
-
-// const todoElem = document.createElement("div");
-// todoElem.className = "todo";
-// todoElem.dataset.id = todo.id;
-// todoElem.dataset.completed = todo.completed;
-// todoElem.innerHTML = `<p>${todo.title}</p> <i class="fa-solid fa-xmark"></i>`;
