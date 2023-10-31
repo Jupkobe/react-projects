00c4cda93e56c96528b81bf3cd94639b1938e01e
Jupkobe <eyupglt42@gmail.com>
2023-10-31 16:18:44 +0300

    b

--- todo-react-tailwind/src/components/Todo.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-
-export default function Todo({ id, completed, title}) {
-    return (
-        <div id={id} className={completed ? "todo completed" : "todo"}>
-            <p>{title}</p> <button className="bg-transparent text-2xl border-0 before:content-['\2715']"></button>
-        </div>
-    )
-}
