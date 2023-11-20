89bcc855f8a572ae2c082f37b53760db3f40d0be
Eyüp <84783072+Jupkobe@users.noreply.github.com>
2023-11-20 19:48:05 +0300

    Update Navbar.jsx

--- superxox/src/components/Navbar.jsx
+++ superxox/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
       <section className='flex items-center justify-center max-w-5xl p-4 mx-auto'>
         <div className='flex items-center justify-center gap-2'>
           <h1 className='text-4xl font-medium text-[#0b0d40] font-roboto'>SUPER</h1>
-          <div className='w-1/4'>
+          <div className='w-1/5'>
             <img src={logo} className="logo" alt="super-tictactoe logo" />
           </div>
         </div>
